7ac5c9974a43cd4f85714f449817546e9402287e
Jeremy Ellis <jeremyellis173@gmail.com>
2026-03-27 18:14:55 -0400

    chore: buttons for testing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../state/useAuthStore";
 
 export default function Home() {
-  const { user } = useAuthStore();
+  const { user, logout } = useAuthStore();
+  const navigate = useNavigate();
+
   return (
     <div>
       <p>{user?.email ?? "no account"}</p>
       <button onClick={() => console.log(user)}>log auth</button>
+      <button onClick={() => navigate("/account")}>account</button>
+      <button onClick={async () => await logout()}>logout</button>
     </div>
   );
 }
